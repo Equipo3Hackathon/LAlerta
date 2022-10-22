23902512d4a57625fed5b919a89b99bef357916b
andreswingu <andres@winguweb.org>
2022-10-22 16:03:51 -0500

    alerts

--- app/Http/Controllers/AlertController.php
+++ app/Http/Controllers/AlertController.php
@@ -32,6 +32,8 @@ class AlertController extends Controller
             'Contactos' => ['recs6anTSWvsW3p9Q', 'reclIIAwI6hmdaBmo'],
         );*/
         log::info($data);
+        $image = json_decode($data['Image']);
+        log::info($image);
         return "OK";
         log::info($data['Contactos']);
         $contactos_ids = $data['Contactos'];
